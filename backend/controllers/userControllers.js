1a274e9987d0ccd1ff55ba893f85fc2d5d9cedc7
Lucas Farias <fariaslucas003@gmail.com>
2023-10-13 17:12:01 -0300

    Endpoints Chat

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -5,7 +5,7 @@ const generateToken = require("../config/generateToken");
 const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password, pic } = req.body;
   
-    if (!name || !email || !password) {
+    if (!name || !email || !password) { 
       res.status(400);
       throw new Error("Por favor, preencha todos os campos");
     }
@@ -59,4 +59,17 @@ const registerUser = asyncHandler(async (req, res) => {
     }
   });
 
-module.exports = { registerUser, authUser };
+  // /api/user?search=lucas <- query
+const allUsers = asyncHandler(async (req,res) => {
+  const keyword = req.query.search ? {
+    $or: [
+      { name: { $regex: req.query.search, $options: "i" } },
+      { email: { $regex: req.query.search, $options: "i" } },
+    ],
+  }: {};
+
+  const users = await User.find(keyword).find({ _id: { $ne: req.user._id } });
+  res.send(users);
+});
+
+module.exports = { registerUser, authUser, allUsers };
